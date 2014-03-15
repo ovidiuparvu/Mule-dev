57c0432ee0cfc506a4065da80414130570e7b30f
Ovidiu Parvu <ovidiu.parvu@gmail.com>
2014-03-15 17:42:59 +0000

    Add script for merging multiple timepoint xml files into a single spatial temporal trace file.

---
+++ Multiscale/script/verification/MergeSpatialTemporalTimePoints.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+###########################################################
+#
+#
+# A script for merging multiple timepoints into a single
+# spatial temporal trace.
+#
+# Each timepoint is represented by a different xml file.
+# The order of merging timepoints is the natural order
+# of the xml files.
+#
+#
+###########################################################
+
+# Check if the required number of parameters was provided
+if [ $# -ne 4 ];
+then
+    echo "Please run the script with the reguired parameters: MergeSpatialTemporalTimePoints.sh <input-folder> <xml-lines-before-timepoint-content> <xml-lines-after-timepoint-content> <output-path>.";
+    
+    exit 1;
+fi
+
+# Check if the first parameter points to a folder
+if [ ! -d $1 ];
+then
+    echo "The input folder path is invalid. Please change.";
+
+    exit 1;
+fi
+
+# Check if the second and third parameters are non-negative integers
+numericPattern='^(0|-?[1-9]([0-9]+)?)$';
+
+if ! [[ $2 =~ ${numericPattern} ]] || ! [[ $3 =~ ${numericPattern} ]];
+then
+    echo "The number of lines before/after timepoint content should be numeric values.";
+
+    exit 1;
+fi
+
+# Inform user of the next action
+echo "Merging the input files...";
+
+# Start the timer for measuring the total execution time
+startTime=$(date +%s.%N);
+
+# Store input parameters in variables
+inputFolder=$1;
+linesBeforeTimePointContent=$2;
+linesAfterTimePointContent=$3;
+outputPath=$4;
+
+# Create the resulting file
+fileCount=`find ${inputFolder} -name "*.xml" | wc -l`;
+
+if [[ ${fileCount} -gt 0 ]];
+then
+    sampleFilePath=`find ${inputFolder} -name "*.xml" | head -n1`;
+
+    # Print the header to the resulting file
+    cat ${sampleFilePath} | head -n ${linesBeforeTimePointContent} > ${outputPath};
+
+    # Process each input file
+    for file in `find ${inputFolder} -name "*.xml" | sort -V`;
+    do
+        cat ${file} | head -n -${linesAfterTimePointContent} | tail -n +$((${linesBeforeTimePointContent} + 1)) >> ${outputPath};
+    done 
+
+    # Print the footer to the resulting file
+    cat ${sampleFilePath} | tail -n ${linesAfterTimePointContent} >> ${outputPath};
+fi
+
+# End the timer for measuring the total execution time
+endTime=$(date +%s.%N);
+
+# Print the total execution time
+echo
+echo "Total execution time: " $(echo "${endTime} - ${startTime}" | bc) " seconds.";
