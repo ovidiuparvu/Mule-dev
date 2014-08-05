5b146dfd3a3cffd95e7112b7ab1540a96499b284
Ovidiu Parvu <ovidiu.parvu@gmail.com>
2014-08-05 17:11:43 +0100

    Add spatial_description xml and xml schema (xsd) files to the project. Add the MSTML_L1V1.xsd file to the project. Update the master.cfg Buildbot script such that the Linux-x64 slave removes all build files before compilation. Add an XmlValidatorSample source file to the project and update CMake accordingly.

---
+++ Multiscale/modules/util/sample/XmlValidatorSample.cpp
@@ -0,0 +1,40 @@
+#include "multiscale/exception/ExceptionHandler.hpp"
+#include "multiscale/util/XmlValidator.hpp"
+
+#include <iostream>
+
+using namespace multiscale;
+
+
+// Check if the given xml file is valid according to the xml schema
+void checkIfValidXmlFile(const std::string &xmlFilepath, const std::string &xmlSchemaFilepath) {
+    std::cout << "The xml file is valid according to the given schema: "
+              << (((XmlValidator::isValidXmlFile(xmlFilepath, xmlSchemaFilepath)) == true)
+                  ? "true" : "false")
+              << std::endl;
+}
+
+// Validate xml file considering the provided arguments
+void validateXmlFile(int argc, char **argv) {
+    try {
+        std::string xmlFilePath     = std::string(argv[1]);
+        std::string xmlSchemaPath   = std::string(argv[2]);
+
+        checkIfValidXmlFile(xmlFilePath, xmlSchemaPath);
+    } catch (const std::exception &ex) {
+        ExceptionHandler::printErrorMessage(ex);
+    }
+}
+
+// Main function
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cerr << "Usage: XmlValidatorSample <path-to-xml> <path-to-xml-schema>" << std::endl;
+
+        return 1;
+    } else {
+        validateXmlFile(argc, argv);
+
+        return EXEC_SUCCESS_CODE;
+    }
+}
